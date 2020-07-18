344d8730b7c9e6f15c13c3f68a2a374b65ae8a9a
Kervens Jasmin <jakejasminjk@gmail.com>
2020-07-18 04:19:25 -0400

    Added State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
+import SignIn from './components/Sign'
+import Show from './components/Show'
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '@material-ui/core/Link';
 //import Container from '@material-ui/core/Container';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {stateVal: 'NONE'};
+
+    this.handleState = this.handleState.bind(this);
+  }
+  handleState(state){
+    this.setState({ stateVal: state }, () => {
+    console.log(this.state.stateVal, 'state is set');
+  });
+  }
+
   render(){
     return (
-      <React.Fragment>
-        <CssBaseline />
-        <Nav name="APPH"/>
-        <Main />
-    </React.Fragment>
-
+      <Router>
+        <React.Fragment>
+          <CssBaseline />
+          <Nav name="APPH"/>
+          <Route exact path="/" render={props =>(
+          <Main />
+          )}/>
+          <Route path="/info" render={props =>(
+          <React.Fragment>
+          <SignIn stateVal= {this.state.stateVal} handleState={this.handleState}/>
+          </React.Fragment>
+          )}/>
+          <Route path="/info/rev" render={props =>(
+          <React.Fragment>
+          <Show stateVal= {this.state.stateVal}/>
+          </React.Fragment>
+          )}/>
+      </React.Fragment>
+     </Router>
   )};
 }
 
